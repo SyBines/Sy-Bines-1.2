28a08bc567c6a71d9a7a81af1db1bb1cb0ca2516
SyBines <seymourb5@nycstudents.net>
2024-09-22 23:57:35 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main
+    {
+        public static void main(String[] args)
+        { 
+        int i = 3;
+        boolean f = true;
+        double l = 3.0;
+        String b = "Hi How are you";
+        i = i += 3;
+
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(l);
+        System.out.println(b);
+        }
+    }
